6d6ccfe6c4859eea88e67dc93d35c11ef809218f
brent.morine <brent.morine@WIR243-5450-NS>
2018-01-17 11:27:09 -0400

    added new window

--- SQLitePython/GUI.py
+++ SQLitePython/GUI.py
@@ -2,7 +2,6 @@ from tkinter import *
 from SQLitePython import *
 import re
 
-
 root = Tk()
 root.geometry('300x300')
 root.title("Learning")
@@ -11,11 +10,14 @@ frame.pack()
 
 #bottomframe = Frame(root)
 #bottomframe.pack( side = BOTTOM )
-
+def anotherwindow():
+    root2 = Tk()
+    root2.geometry('300x300')
+    root2.title("Learning Window Two")
+    root.mainloop()
 
 def updateinfo():
     text = e1.get()
-    label = Label(frame, text=text)
     label.pack()
     findinfo(rows)
 
@@ -24,8 +26,8 @@ def findinfo(rows):
     for row in rows:
         result = re.search(text, row[1])
         if result:
-            newlabel = Label(frame, text="true")
-            newlabel.pack()
+             newlabel = Label(frame, text="true")
+             newlabel.pack()            
         
     
 if __name__ == '__main__':
@@ -39,9 +41,12 @@ if __name__ == '__main__':
 e1 = Entry(frame , width=50)
 e1.pack()  
 blackbutton = Button(frame, text="FIND", background="black", fg="white", command=updateinfo)
+blackbutton2 = Button(frame, text="New Window", background="black", fg="white", command=anotherwindow)
 blackbutton.pack( side = BOTTOM)
+blackbutton2.pack( side = BOTTOM)
 
 #menubar = Menu(frame, tearoff=0)
 #menubar.pack(side = BOTTOM)
+
 
 root.mainloop()
